940da34aefdfc892e06548913926e8bcb6ada7b0
Kuranasaki@M3Max <ironpan10@hotmail.com>
2026-05-15 21:26:51 +0700

    Make Jai work

--- route/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.sticker = sticker = require('./sticker')
-module.exports.music = music = require('./music')
-module.exports.activity = activity = require('./activity')
-module.exports.tts = tts = require('./tts')
